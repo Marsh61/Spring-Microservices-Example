19e1916708250156b4980ab0f43be096ca382a5a
Brandon <brandonmarshall94@gmail.com>
2018-03-06 19:39:42 -0500

    cookbooks uploaded correctly

---
+++ Chef/learn-chef/cookbooks/microservices/README.md
@@ -0,0 +1,4 @@
+# microservices
+
+TODO: Enter the cookbook description here.
+
---
+++ Chef/learn-chef/cookbooks/microservices/recipes/default.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook:: microservices
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
+template 'C:\Users\Administrator\Desktop\MalwareServer\public\index.html' do
+    source 'index.html.erb'
+end
+
+
